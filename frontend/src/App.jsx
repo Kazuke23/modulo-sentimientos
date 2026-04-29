af9a22b4e3a14dceac3a2cb1533b3cca6b7898a2
Kazuke23 <sebastian.buitrago01@unicatolica.edu.co>
2026-04-28 21:18:45 -0500

    fix: actualizar URL ngrok 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,7 +20,7 @@ export default function App() {
     setError("")
     setResultado(null)
     try {
-      const res = await axios.post("https://icy-overfill-footpad.ngrok-free.dev", {
+      const res = await axios.post("https://icy-overfill-footpad.ngrok-free.dev/analizar", {
         url,
         max_comentarios: maxComentarios,
       })
